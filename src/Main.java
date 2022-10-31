1ef0667b56311a1a9e5bf3ee7dd861eb74bfe3cf
User <dzhumanazaruulu@gmail.com>
2022-10-31 18:04:30 +0600

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(method(4, 2,3,4,2,1,7));
+    }
+    public static boolean method(int a, int... numbers) {
+        for (int i : numbers) {
+            if (a == i) {
+                return true;
+            }
+        }
+        return false;
     }
 }
